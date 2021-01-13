f54d8eae959a2ce10c234a55d4a1281de7d90836
Mark Heckler <mark.heckler@gmail.com>
2021-01-12 18:59:34 -0600

    Initial commit

---
+++ src/main/java/com/thehecklers/actuatorstartuptest/ActuatorStartupTestApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.actuatorstartuptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorStartupTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorStartupTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thehecklers/actuatorstartuptest/ActuatorStartupTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.actuatorstartuptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorStartupTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
